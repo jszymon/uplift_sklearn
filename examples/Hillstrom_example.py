63a77dea9475bbe9dfe9ad99ca44f56688c42485
SJ <jszymon@gmail.com>
2018-08-11 15:48:49 +0200

    encode Hillstrom data as floats

--- examples/Hillstrom_example.py
+++ examples/Hillstrom_example.py
@@ -6,7 +6,26 @@ Hillstrom
 Build uplift classification and regression models on Hillstrom data.
 """
 
+import numpy as np
+
 from usklearn.datasets import fetch_Hillstrom
 
+def encode_features(D):
+    """Convert features to float matrix.
+
+    Use K-1 encoding for categorical variables."""
+    X = D.data
+    cols = []
+    for c in D.feature_names:
+        if c not in D.categ_values:
+            cols.append(np.asfarray(X[c]))
+        else:
+            n_categs = len(D.categ_values[c])
+            x = np.eye(n_categs)[X[c]]
+            cols.append(x[:,:-1]) # skip last category
+    return np.column_stack(cols)
+
+
 D = fetch_Hillstrom()
-print(D)
+X = encode_features(D)
+print(X)
