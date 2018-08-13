5d47e9f90c1db3c4b3d1214e5fd91445b47b88cc
SJ <jszymon@gmail.com>
2018-08-13 16:18:00 +0200

    add estimator type to uplift regressor add a function to test if estimator is an uplift model

--- usklearn/base.py
+++ usklearn/base.py
@@ -5,5 +5,23 @@ from sklearn.base import BaseEstimator
 from .metrics import e_sate
 
 class UpliftRegressorMixin(object):
+    _estimator_type = "regressor"
+    _uplift_model = True
+
     def score(self, X, y, trt, sample_weight=None):
         return e_sate(y, self.predict(X), trt, n_trt=self.n_trt_)
+
+def is_uplift(estimator):
+    """Returns True if the given estimator is an uplift model.
+
+    Parameters
+    ----------
+    estimator : object
+        Estimator object to test.
+
+    Returns
+    -------
+    out : bool
+        True if estimator is an uplift model and False otherwise.
+    """
+    return getattr(estimator, "_uplift_model", None) is True
