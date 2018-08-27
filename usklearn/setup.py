a7b4d9548cd223d5ce27f791a4fe52849394784b
DESKTOP-JOMVOIF\Krzysztof Rudaś <krzysztof.rudas@gmail.com>
2018-08-27 14:25:31 +0200

    setup file

---
+++ usklearn/setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import setup
+
+setup(name='usklearn',
+      version='0.1',
+      description='Uplift scikit learn',
+      url='http://github.com/jszymon/uplift_sklearn/tree/master/usklearn',
+      author='Szymon Jaroszewicz, Krzysztof Rudas',
+      author_email='krzysztof.rudas@ipipan.waw.pl',
+      license='MIT',
+      packages=['usklearn'],
+      zip_safe=False)
