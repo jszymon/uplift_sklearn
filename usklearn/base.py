f2b6345d38d269c7c0531e463f79148f32d4d7b7
SJ <jszymon@gmail.com>
2019-01-06 11:49:31 +0100

    add uplift transformer mixin

--- usklearn/base.py
+++ usklearn/base.py
@@ -21,6 +21,19 @@ class UpliftRegressorMixin(object):
             a[best_y <= 0] == 0
         return a
 
+class UpliftTransformerMixin(object):
+    def fit_transform(self, X, trt, n_trt=None, y=None, **fit_params):
+        # non-optimized default implementation; override when a better
+        # method is possible for a given clustering algorithm
+        if y is None:
+            # fit method of arity 1 (unsupervised transformation)
+            fitted = self.fit(X, trt, n_trt, **fit_params)
+            return fitted.transform(X, trt, n_trt)
+        else:
+            # fit method of arity 2 (supervised transformation)
+            fitted = self.fit(X, trt, n_trt, y, **fit_params)
+            return fitted.transform(X, trt, n_trt)
+
 def is_uplift(estimator):
     """Returns True if the given estimator is an uplift model.
 
