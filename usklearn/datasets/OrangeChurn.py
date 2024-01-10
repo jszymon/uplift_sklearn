c46cb052a6734416e99a0d94db74a3fd63c956f8
Szymon Jaroszewicz <jszymon@gmail.com>
2024-01-10 13:58:30 +0100

    Organge churn dataset: add return_X_y option

--- usklearn/datasets/OrangeChurn.py
+++ usklearn/datasets/OrangeChurn.py
@@ -2,8 +2,11 @@ import numpy as np
 
 from sklearn.datasets import fetch_openml
 
-def fetch_Orange_churn():
-    D = fetch_openml("churn-uplift-orange", parser="auto")
+def fetch_Orange_churn(return_X_y=False, **args):
+    D = fetch_openml("churn-uplift-orange", parser="auto", **args)
     D.trt = np.asarray(D.data.t, dtype=np.int32)
     D.data = D.data.drop(columns="t")
+    D.feature_names.remove("t")
+    if return_X_y:
+        return D.data, D.target.values.astype(np.int32), D.trt
     return D
