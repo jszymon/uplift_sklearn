b6dc098267273c4dd119386ad172985ad480f36c
Szymon Jaroszewicz <jszymon@gmail.com>
2024-01-11 16:35:15 +0100

    add tratment values to Ogrange Churn dataset

--- usklearn/datasets/OrangeChurn.py
+++ usklearn/datasets/OrangeChurn.py
@@ -7,6 +7,7 @@ def fetch_Orange_churn(return_X_y=False, **args):
     D.trt = np.asarray(D.data.t, dtype=np.int32)
     D.data = D.data.drop(columns="t")
     D.feature_names.remove("t")
+    D.treatment_values = ["0", "1"]
     if return_X_y:
         return D.data, D.target.values.astype(np.int32), D.trt
     return D
