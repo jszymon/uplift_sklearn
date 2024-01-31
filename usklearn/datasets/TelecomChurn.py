a35c652da339ebe7beb615d0e959137965f97c0a
Szymon Jaroszewicz <jszymon@gmail.com>
2024-01-31 20:21:20 +0100

    remove Telco name from telecom churn dataset

--- usklearn/datasets/TelecomChurn.py
+++ usklearn/datasets/TelecomChurn.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.datasets import fetch_openml
 
 def fetch_Orange_churn(return_X_y=False, **args):
-    D = fetch_openml("churn-uplift-orange", parser="auto", **args)
+    D = fetch_openml("churn-uplift-mlg", parser="auto", **args)
     D.trt = np.asarray(D.data.t, dtype=np.int32)
     D.data = D.data.drop(columns="t")
     D.feature_names.remove("t")
