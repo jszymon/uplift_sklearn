b26b296be1e8f9e46740acec470f0d920bb1430a
Szymon Jaroszewicz <jszymon@gmail.com>
2024-02-29 00:47:50 +0100

    more models in Hillstrom example add Telecom Churn example

--- examples/TelecomChurn_uplift.py
+++ examples/TelecomChurn_uplift.py
@@ -3,7 +3,7 @@
 Hillstrom
 =========
 
-Build uplift classification and regression models on Hillstrom data.
+Build uplift classification and regression models on Telecom churn data.
 """
 
 import numpy as np
@@ -11,10 +11,11 @@ import numpy as np
 from sklearn.base import is_classifier
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-from usklearn.datasets import fetch_Hillstrom
+from usklearn.datasets import fetch_TelecomChurn
 from usklearn.meta import MultimodelUpliftRegressor
 from usklearn.meta import MultimodelUpliftClassifier
 from usklearn.meta import TreatmentUpliftClassifier
@@ -26,6 +27,10 @@ from usklearn.model_selection import cross_validate, cross_val_score, uplift_che
 
 import matplotlib.pyplot as plt
 
+try:
+    from tqdm import tqdm
+except:
+    tqdm = lambda x: x
 
 def encode_features(D):
     """Convert features to float matrix.
@@ -34,37 +39,33 @@ def encode_features(D):
     X = D.data
     cols = []
     for c in D.feature_names:
-        if c not in D.categ_values:
+        if not c.startswith("FACTOR"):
             cols.append(np.asfarray(X[c]))
         else:
-            n_categs = len(D.categ_values[c])
-            x = np.eye(n_categs)[X[c]]
-            cols.append(x[:,:-1]) # skip last category
+            x = OneHotEncoder(sparse_output=False, drop='first').fit_transform(X[[c]])
+            cols.append(x)
     return np.column_stack(cols)
 
-D = fetch_Hillstrom(as_frame=True)
+D = fetch_TelecomChurn(as_frame=True)
 X = encode_features(D)
-y = D.target_visit
+y = D.target
 trt = D.treatment
-# keep women's campaign
-mask = ~(trt == 1)
-X = X[mask]
-y = y[mask]
-trt = (trt[mask] == 2)*1
 n_trt = 1
 
-n_iter = 100
+n_iter = 10
 
 base_classifier = Pipeline([("scaler", StandardScaler()),
-                            ("logistic", LogisticRegression(max_iter=100))])
-models = [MultimodelUpliftRegressor(),
+                            ("logistic", LogisticRegression(max_iter=1000, C=0.1, random_state=234))])
+#base_classifier = RandomForestClassifier(n_estimators=10)
+models = [#MultimodelUpliftRegressor(),
           MultimodelUpliftClassifier(base_estimator=base_classifier),
-          TreatmentUpliftClassifier(base_estimator=base_classifier),
+          TreatmentUpliftClassifier(base_estimator=base_classifier, reverse=False),
+          TreatmentUpliftClassifier(base_estimator=base_classifier, reverse=True),
           ResponseUpliftClassifier(base_estimator=base_classifier),
           ControlUpliftClassifier(base_estimator=base_classifier, reverse=True),
           ControlUpliftClassifier(base_estimator=base_classifier, reverse=False),
           ]
-cv, y_stratify = uplift_check_cv(StratifiedShuffleSplit(test_size=10000,
+cv, y_stratify = uplift_check_cv(StratifiedShuffleSplit(test_size=3000,
                                                         n_splits=n_iter,
                                                         random_state=123),
                                  y, trt, n_trt, classifier=True)
@@ -73,13 +74,14 @@ colors = "rgbkcy"
 avg_x = np.linspace(0,1,1000)
 avg_u = np.zeros((len(models), len(avg_x)))
 
-for train_index, test_index in cv.split(X, y_stratify):
+for train_index, test_index in tqdm(cv.split(X, y_stratify)):
     for mi, m in enumerate(models):
         m.fit(X[train_index], y[train_index], trt[train_index], n_trt)
+        #test_index = train_index
         score = m.predict(X[test_index])
         if is_classifier(m):
-            score = score[:,1]
-        x, u = uplift_curve(y[test_index], score, trt[test_index], n_trt)
+            score = score[:,0]
+        x, u = uplift_curve(y[test_index], score, trt[test_index], n_trt, pos_label=0)
         plt.plot(x, u, color=colors[mi], alpha=0.05)
 
         avg_u[mi] += np.interp(avg_x, x, u)
