a35481809a3bcdf146d9d77d9315d71ae59fadf0
Szymon Jaroszewicz <jszymon@gmail.com>
2024-07-11 22:19:47 +0200

    expose iter_quantiles in metrics

--- usklearn/metrics/__init__.py
+++ usklearn/metrics/__init__.py
@@ -1,6 +1,7 @@
 from .regression import e_sate, e_satt
 from .curves import uplift_curve, uplift_curve_j
 from .curves import area_under_uplift_curve, area_under_uplift_curve_j
+from .bins import iter_quantiles
 from .bins import QMSE, QMSE_j, EUCE, MUCE
 from ._scorer import (
     make_uplift_scorer,
