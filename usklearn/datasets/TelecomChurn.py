6428f8b56f29ba5fa563d6f846f0443d219edbfc
Szymon Jaroszewicz <jszymon@gmail.com>
2024-09-22 09:12:49 +0200

    add target_names to TelecomChurn data

--- usklearn/datasets/TelecomChurn.py
+++ usklearn/datasets/TelecomChurn.py
@@ -31,6 +31,7 @@ def fetch_TelecomChurn(return_X_y=False, **args):
     D.treatment_values = ["0", "1"]
     D.n_trt = 1
     D.target = D.target.values.astype(np.int32)
+    D.target_names = ["target"]
     D.categ_values = categ_values
     if return_X_y:
         return D.data, D.target, D.trt
